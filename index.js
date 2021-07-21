2b4048b099b5432849fa0c019750a5e958c613c4
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2021-07-21 13:31:12 +0100

    Completed kata

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function solution(string) {
-    
-}
+const solution = (string) => {
+	let text = '';
 
-// "camelCasing"  =>  "camel Casing"
-// "identifier"   =>  "identifier"
-// ""             =>  ""
+	try {
+		string.match(/[A-z]/g).map((char) => {
+			text += char.match(/[A-Z]/g) ? ` ${char}` : char;
+		});
+	} catch (error) {
+		return '';
+	}
+
+	return text.trim();
+};
+
+solution('camelCasing');
+console.log(solution('camelCasing'));
+solution('camelCasingTest');
+console.log(solution('camelCasingTest'));
+solution('identifier');
+console.log(solution('identifier'));
+solution('');
+console.log(solution(''));
